4964e0305f0aaf705ec694de63d1ea8e5321a160
William-dev2 <arnoldtoukam33@gmail.com>
2025-09-24 19:23:51 +0100

    Update produit_matrices.h

---
+++ 2.produit_matrices/produit_matrices.h
@@ -0,0 +1,8 @@
+#ifndef MATRICES_PRODUIT_H
+#define MATRICES_PRODUIT_H
+
+void lireMatrice(int matrice[10][10], int lignes, int colonnes);
+void multiplierMatrices(int matrice1[10][10], int matrice2[10][10], int resultat[10][10], int lignes1, int colonnes1, int colonnes2);
+void afficherMatrice(int matrice[10][10], int lignes, int colonnes);
+
+#endif
